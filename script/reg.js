3018f046d299502431dd214986bb89b3837e23b6
Mikhail <mrgolubev@edu.hse.ru>
2025-06-10 17:11:49 +0500

    папка для js

---
+++ script/reg.js
@@ -0,0 +1,6 @@
+document.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const login = document.querySelector('#loginInput');
+    const pass = document.querySelector('#passwordInput');
+    
+})
